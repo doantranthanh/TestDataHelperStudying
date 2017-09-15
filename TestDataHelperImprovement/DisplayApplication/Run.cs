7aefa08abc8b03ce26b678604e51f4c7189a3997
thanhdoan <thanh.doan1@rbi.co.uk>
2017-09-15 11:43:41 +0100

    Changes

--- TestDataHelperImprovement/DisplayApplication/Run.cs
+++ TestDataHelperImprovement/DisplayApplication/Run.cs
@@ -18,6 +18,8 @@ namespace DisplayApplication
                 a.City = "Paris";
                 return a;
             }).Build();
+
+            Console.WriteLine(address.City);
         }
     }
 }
